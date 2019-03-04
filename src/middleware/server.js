297ffb0c60dfcea5cce40eaf2c7ef6108b6f112c
Benjamin J Anderson <banderson@mashable.com>
2019-03-04 08:30:09 -0600

    Add default client event handler

--- src/middleware/server.js
+++ src/middleware/server.js
@@ -1,3 +1,14 @@
+/**
+  * Socketio Redux Middleware Server Event Handler.
+  * @module socketio-middleware/middleware/server
+  */
+
+/**
+  * Event handler for server events
+  * @param {array} serverEvents - the user server events list
+  * @param {function} dispatch - the redux dispatch function
+  * @since v0.0.1
+  */
 export default function serverEventHandler(serverEvents, dispatch) {
   return (event, data) => {
     serverEvents.some((e) => {
