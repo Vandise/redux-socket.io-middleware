8f64ba84359005cf8b3f8aab053f33a34df8d132
Plouffee <plouffehawk@gmail.com>
2020-08-19 10:22:47 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -146,9 +146,9 @@ export function generateConnectString(payload) {
   if (payload.namespace) {
     connStr += `/${payload.namespace}`;
   }
- if (!payload.port) {
+  if (!payload.port) {
     connStr = `${payload.host}`;
- }
+  }
   return connStr;
 };
 
